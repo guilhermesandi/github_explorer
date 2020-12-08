1eee49ee1ad56727275f4fe453f61702427411e9
Guilherme Sandi <guilherme_sandi@hotmail.com>
2020-12-08 12:38:33 -0300

    Interface Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/38008649?s=460&u=039f3469bbecaa8d4a626f3d53a70c7fb986282c&v=4"
+            alt="Guilherme Sandi"
+          />
+          <div>
+            <strong>guilhermesandi/happy</strong>
+            <p>Happy | Leve felicidade para o mundo</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
